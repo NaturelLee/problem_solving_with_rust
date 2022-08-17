d895b7f4f5102dd0f4fdbb6ee611e71d5285a576
Naturel Lee <1314carefully@gmail.com>
2022-08-17 17:22:28 +0800

    feat: add disorder string with chars count method

--- src/chapter2/disorder_strings.rs
+++ src/chapter2/disorder_strings.rs
@@ -45,6 +45,24 @@ fn disorder_strings(s1: &str, s2: &str) -> bool {
     t1 == t2
 }
 
+fn disorder_strings_char_count(s1: &str, s2: &str) -> bool {
+    if s1.len() != s2.len() {
+        return false;
+    }
+    let mut a1 = [0; 26];
+    let mut a2 = [0; 26];
+
+    for c in s1.chars() {
+        a1[c as usize - 97] += 1;
+    }
+
+    for c in s2.chars() {
+        a2[c as usize - 97] += 1;
+    }
+
+    a1 == a2
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -64,6 +82,17 @@ mod tests {
         assert!(anagram_solution("abc", "cba"));
         assert!(anagram_solution("", ""));
         assert!(anagram_solution(
+            "abcdefghijklmnopqrstuvwxyz",
+            "abcdeklmnxyzopqrstufghijvw"
+        ));
+    }
+
+    #[test]
+    fn test_disorder_strings_char_count() {
+        assert!(disorder_strings_char_count("abc", "acb"));
+        assert!(disorder_strings_char_count("abc", "cba"));
+        assert!(disorder_strings_char_count("", ""));
+        assert!(disorder_strings_char_count(
             "abcdefghijklmnopqrstuvwxyz",
             "abcdeklmnxyzopqrstufghijvw"
         ));
